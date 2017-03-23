c0c87df886f18bb544749e5774391ed95701201e
KRISHNA\CHANDAN <chandan115325@gmail.com>
2017-03-23 17:00:19 +0530

    session_01_java_assignment no 4 is done.

---
+++ src/session_01_java_assignment_04/PrintCharFromASCII.java
@@ -0,0 +1,15 @@
+package session_01_java_assignment_04;
+
+public class PrintCharFromASCII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=65;
+		while(a<=90)
+		{
+		System.out.println(a+" - "+(char)a);
+		a++;
+		}
+	}
+
+}
